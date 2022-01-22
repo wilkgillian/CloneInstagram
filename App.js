f588c6c8b0454f14ee6f9b4925d4df79ba80729e
Wilk Gillian <92535322+wilkgillian@users.noreply.github.com>
2022-01-22 19:29:59 -0400

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import { Header } from './components/Header'
+import { Layout } from './components/Layout'
+import './styles/globals.css'
+function App() {
+  return (
+    <>
+      <Header />
+      <Layout />
+    </>
+  )
+}
+
+export default App
