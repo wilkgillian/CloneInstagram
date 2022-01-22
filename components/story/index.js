cb1446a76d7e4f6dc0b82cd0aefc6b6922a56fe3
Wilk Gillian <92535322+wilkgillian@users.noreply.github.com>
2022-01-22 19:35:59 -0400

    Add files via upload

---
+++ components/story/index.js
@@ -0,0 +1,27 @@
+import './style.css'
+export function Story() {
+  return (
+    <div className="container">
+      <div className="user-elements">
+        <div>
+          <img
+            className="image-user-story"
+            src="https://github.com/peas.png"
+            alt="img not found"
+          />
+        </div>
+        <span>neymarjr</span>
+      </div>
+      <div className="user-elements">
+        <div>
+          <img
+            className="image-user-story"
+            src="https://github.com/peas.png"
+            alt="img not found"
+          />
+        </div>
+        <span>neymarjr</span>
+      </div>
+    </div>
+  )
+}
